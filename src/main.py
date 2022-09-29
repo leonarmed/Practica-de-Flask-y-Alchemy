795a9fa8c21d79e4b38a8667e6cfa878273a2ee6
Leonardo Medina <leonarmed@gmail.com>
2022-09-29 00:05:17 +0000

    Proceso completo clase

--- src/main.py
+++ src/main.py
@@ -39,24 +39,91 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
-@app.route('/contact', methods=['POST'])
-def create_contact():
-    """
-    Create new contacts
-    """
-    body = request.json
-    new_contact = Contacts()
-    new_contact.name = body["name"]
-    new_contact.email = body["email"]
-    new_contact.phone = body["phone"]
-    new_contact.active = body["active"]
+# @app.route('/contact', methods=['POST'])
+# def create_contact():
+#     """
+#     Create new contacts
+#     """
+#     body = request.json
+#     new_contact = Contacts()
+#     new_contact.name = body["name"]
+#     new_contact.email = body["email"]
+#     new_contact.phone = body["phone"]
+#     new_contact.active = body["active"]
     
-    db.session.add(new_contact)
-    db.session.commit()
+#     db.session.add(new_contact)
+#     db.session.commit()
 
-    print(new_contact)
-    print(body)
-    return "Hello", 201
+#     print(new_contact)
+#     print(body)
+#     return "Hello", 201
+
+# @app.route("/contact", methods=['POST'])
+# def create():
+#     body = request.json
+#     print(body)
+#     new_contact = Contacts.create(body["name"], body["email"], body["phone"], body["active"])
+#     if new_contact["success"] and isinstance(new_contact, Contacts):
+#         saved = Contacts.save_and_commit(new_contact)
+#         if not saved:
+#             return jsonify({
+#                 "message":"Error interno de la App",
+#             }), 500
+#         return jsonify([]), 201
+    
+#     return jsonify({
+#         "message":"No se pudo crear"
+#     }), 500
+
+@app.route("/contact", methods=["GET", "POST"])
+def get_contact():
+    if request.method == "GET":
+        contact_list = Contacts.query.all()
+        print(contact_list)
+        contacts_dict = list(map(lambda contact: contact.serialize(), contact_list))
+        print(contacts_dict)
+        return jsonify({
+            "success": True,
+            "contacts": contacts_dict
+        }), 200
+    if request.method == "POST":
+        """ 
+        Crea una instancia y la almacena en la base de datos. En caso ocurra un error en el proceso, se returna el error correspondiente
+        """
+        body = request.json
+        new_contact = Contacts.create(body) #Creamos la instancia ejecutando el método de clase creado en models
+
+        if not isinstance(new_contact, Contacts): #Si no es una instancia de Contact, quiere decir que ocurrió un error
+            print(new_contact)
+            return jsonify({
+                "message": new_contact["message"]
+            }), new_contact["status"]
+
+        contact = Contacts.query.filter_by(email=new_contact.email).one_or_none()  #Obtenemos el contacto para acceder al id, el cual es generado luego de que la instancia se guarda en la base de datos.
+        return jsonify({
+            "sucess": True,
+            "contact": contact.serialize()
+        }), 201
+
+@app.route("/getcontact/<int:id>")
+def get_contact_by_id(id):
+    try:
+        contact = Contacts.query.get(id)
+        if contact is None:
+            return jsonify({
+                "success": False,
+                "message": "Contact does not exist"
+            }), 404
+        return jsonify({
+            "success": True,
+            "contact": contact.serialize()
+        }), 200
+        print(contact)
+    except Exception as error:
+        return jsonify({
+            "success": False,
+            "message": "Error del servidor"
+        }), 500
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -24,3 +24,109 @@ class Contacts(db.Model):
     email = db.Column(db.String(250), unique=True, nullable=False)
     phone = db.Column(db.String(80))
     active = db.Column(db.String(80), nullable=False)
+
+    @classmethod
+    def create(cls, body):
+        try:
+            validated = cls.data_is_valid(name=body.get("name"), email=body.get("email"), phone=body.get("phone"), active=body.get("active"))
+            if type(validated) == dict: #Si validated es un diccionario, quiere decir que ocurrió un error
+                raise Exception({ #Levantamos un error con la información del problema ocurrio. Este error es referente a la data o el email en específico
+                    "message": validated["message"],
+                    "status": validated["status"]
+                })
+            
+            new_contact = cls(name=body["name"], email=body["email"], phone=body.get("phone", None), active=body.get("active")) #Creamos la instancia del Contacto
+            if not isinstance(new_contact, cls):
+                raise Exception({
+                    "message": "Instance Error",
+                    "status": 500
+                })
+            
+            saved = new_contact.save_and_commit() #Salvamos la instancia creada en la sesion y le hacemos commit para crear el registro
+
+            if not saved:
+                raise Exception({
+                    "message": "Data Base error",
+                    "status": 500
+                })
+            return new_contact
+        except Exception as error:
+            #Retornamos el error que estamos levantando, el cual se estructuró como un diccionario con status y message como keys
+            return error.args[0]
+
+    @classmethod
+    def data_is_valid(cls, **kwargs):
+        """
+            Verifica si la data enviada es correcta y está completa. También se verifica si el email es válido.
+        """
+        try:
+            if kwargs["email"] is None:  #Si no nos enviaron un Email, levantamos un error.
+                raise Exception({
+                    "message": "Missing email",
+                    "status": 400
+                })
+            if kwargs["name"] is None: #Si no nos enviaron un nombre, levantamos un error.
+                raise Exception({
+                    "message": "Missing name",
+                    "status": 400
+                })
+            if kwargs["active"] is None: #Si no nos enviaron un nombre, levantamos un error.
+                raise Exception({
+                    "message": "Verify status user",
+                    "status": 400
+                })
+            email_valid = cls.email_is_valid(kwargs["email"])
+            if email_valid == False:  #Si el email existe en db, levantamos un error.
+                raise Exception({
+                    "message": "Invalid email",
+                    "status": 400
+                })
+            if email_valid != True: #Si, email_valid es distinto de un booleano, existe un error en el servidor.
+                raise Exception({
+                    "message": "Server error",
+                    "status": 500
+                })
+            
+            return True
+        except Exception as error:
+            #Retornamos los errores
+            return error.args[0]
+
+    @classmethod
+    def email_is_valid(cls, new_user_email):
+        """
+            Verifica si el email es válido. Sí el email existe en la base de datos, el email no es válido.
+        """
+        try:
+            user_exist = cls.query.filter_by(email=new_user_email).one_or_none() #Consultamos en  la db si existe un registro con el email. Si existe, lo retorna. En caso contario, retorna None
+            if user_exist:
+                return False
+            return True
+        except Exception as error:
+            return error.args[0]
+
+    def save_and_commit(self):
+        """
+            Salva la instancia creada, en la base de datos. Si sucede algún error, 
+            se retorna False y se revierten los cambios de la sesión
+        """
+        try:
+            db.session.add(self)  #Guardamos la instancia en la sessión
+            db.session.commit() #Creamos el registro en la db 
+            return True
+        except error:
+            db.session.rollback() #Retornamos a la session mas reciente
+            return False
+
+    def serialize(self):
+        """
+            Representa una instancia es un diccionario
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "phone": self.phone,
+            "active": self.active
+        }
+
